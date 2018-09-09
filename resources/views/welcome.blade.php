473599f5c93b76d99a86588753320ac1176ab338
=mariam shqair <=>
2018-09-09 13:09:01 +0300

    Tweak home page copy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                  master page changes in   Laravel home page
                 </div>
 
                 <div class="links">
